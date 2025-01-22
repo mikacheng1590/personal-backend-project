3ae97191b2a72773ed2f9dd1095889ec8758e0ea
Mika Fung <fungcheukman@hotmail.com>
2025-01-22 11:14:08 -0800

    add ping route

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ import 'dotenv/config';
 import express from 'express';
 // import paulasChoiceCronJob from './paulas-choice-cron-job/index.js';
 import paulasChoiceRoutes from './routes/paulas-choice.js';
+import pingRoutes from './routes/ping.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use('/paulas-choice', paulasChoiceRoutes);
+app.use('/ping', pingRoutes);
 
 app.listen(port, () => {
   console.log(`Mika's app listening on port ${port}`);

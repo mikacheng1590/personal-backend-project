39a4f4b7294caa23d6255cb83d68a247db8b4a44
Mika Fung <fungcheukman@hotmail.com>
2025-03-26 14:45:07 -0700

    add basic auth to email service api

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express from 'express';
 import bodyParser from 'body-parser';
+import basicAuth from 'express-basic-auth';
 // import paulasChoiceCronJob from './paulas-choice-cron-job/index.js';
 import paulasChoiceRoutes from './routes/paulas-choice.js';
 import pingRoutes from './routes/ping.js';
@@ -8,11 +9,14 @@ import sendEmailRoutes from './routes/email.js';
 
 const app = express();
 const port = process.env.PORT || 4000;
+const basicAuthMiddleware = basicAuth({
+  users: { [process.env.API_SIMPLE_USERNAME]: process.env.API_SIMPLE_PASSWORD },
+});
 
 app.use(bodyParser.json());
 app.use('/paulas-choice', paulasChoiceRoutes);
 app.use('/ping', pingRoutes);
-app.use('/v1/email-service', sendEmailRoutes);
+app.use('/v1/email-service', basicAuthMiddleware, sendEmailRoutes);
 
 app.listen(port, () => {
   console.log(`Mika's app listening on port ${port}`);
